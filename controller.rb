b998a111b0f0acbfab6eecb57304fb560f52c62c
Git Money & Reid Covington <pair+alexanderdavidpan.reidcovington@devbootcamp.com>
2014-05-02 14:15:50 -0400

    Initial commit. Create program stucture.

---
+++ controller.rb
@@ -0,0 +1,34 @@
+#store spoilers
+#
+class User
+  def initialize
+    @spoilers = []
+    @username =
+    @password =
+  end
+
+end
+
+
+class Authenticate
+  def initialize
+  end
+
+end
+
+
+class Controller
+  def initialize
+  end
+
+
+end
+
+
+
+
+
+
+Controller.new
+
+
---
+++ view.rb
@@ -0,0 +1,5 @@
+class BrowserUI
+  def initialize
+  end
+end
+
