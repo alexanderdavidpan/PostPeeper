8ed459088e61a43d2ca99ac1aa2c17a174b152b4
Git Money & Reid Covington <pair+alexanderdavidpan.reidcovington@devbootcamp.com>
2014-05-02 19:44:20 -0400

    Updated Controller and View. Work in progress

--- view.rb
+++ view.rb
@@ -1,5 +1,47 @@
+# load 'controller.rb'
+
 class BrowserUI
-  def initialize
+  attr_accessor :spoilers_array
+
+  def self.get_spoilers
+    @spoilers_array = []
+    puts "="*50
+    puts "What spoiler subjects would you like to avoid? (separate topics
+      by semicolons"
+    puts "-"*50
+    a = gets.chomp.split("; ")
+    @spoilers_array << a
+    @spoilers_array.flatten!
   end
+
+  def delete_spoilers
+    puts "-"*50
+    puts "What spoiler filter would you like to take out? (separate topics
+      by semicolons"
+    spoiler_to_be_deleted = gets.chomp.split("; ").downcase
+    #spoiler_to_be_deleted.each {|spoiler| @spoilers.delete(spoiler)}
+  end
+
+  def self.display_post
+    puts "---SPOILER FOUND---"
+    puts "What would you like to do? Skip or View?"
+    spoiler_command = gets.chomp.downcase
+    case spoiler_command
+    when "skip"
+      return
+    when "view"
+      true
+    end
+  end
+
+  def acquire_post
+    DatabaseConnector.get_posts
+  end
+
 end
 
+# view = BrowserUI.new
+# p view.spoilers_array
+# # @spoilers.each do |spoiler_subject|
+# #   spoiler_subject =~ /()/
+# # end
